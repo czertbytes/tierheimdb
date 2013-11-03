5b323f9f61ee5dc2dbedac3e6b510327b8338fee
Daniel Hodan <daniel.hodan@czertbytes.de>
2013-11-03 19:15:27 +0100

    Fix Tierheim verlorenwasser

--- catnip/verlorenwasser/parser.go
+++ catnip/verlorenwasser/parser.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/xml"
+	"fmt"
 	"io"
 	"log"
 	"strings"
@@ -29,21 +29,25 @@ func (p *Parser) ParsePagination(r io.Reader) (int, int, int, error) {
 
 func (p *Parser) ParseList(r io.Reader) ([]*pb.Animal, error) {
 	var animals []*pb.Animal
-	var rss cp.RSS
 
-	d := xml.NewDecoder(r)
-	if err := d.Decode(&rss); err != nil {
-		return nil, err
+	doc, err := html.Parse(r)
+	if err != nil {
+		return animals, err
 	}
 
-	for _, i := range rss.Channel.Items {
-		name := cp.NormalizeName(i.Title)
-		animals = append(animals, &pb.Animal{
-			Id:       cp.NormalizeId(name),
-			Name:     name,
-			LongDesc: p.parseLongDesc(i.Description),
-			URL:      i.Link,
-		})
+	for _, animalNode := range p.listAnimalNodes(doc) {
+		name := cp.NormalizeName(p.parseName(animalNode))
+		linkNodes := p.detailLinkNodes(animalNode)
+
+		if len(name) > 0 && len(linkNodes) == 1 {
+			link := cp.NodeAttribute(linkNodes[0], "href")
+			animals = append(animals, &pb.Animal{
+				Id:       cp.NormalizeId(name),
+				Name:     name,
+				LongDesc: p.parseLongDesc(animalNode),
+				URL:      fmt.Sprintf("http://www.tierheim-verlorenwasser.de%s", link),
+			})
+		}
 	}
 
 	return animals, nil
@@ -60,17 +64,32 @@ func (p *Parser) ParseDetail(r io.Reader) (*pb.Animal, error) {
 	}, nil
 }
 
-func (p *Parser) parseLongDesc(text string) string {
+func (p *Parser) parseName(doc *html.Node) string {
+	nameNodes := p.nameNodes(doc)
+	if len(nameNodes) != 1 {
+		return ""
+	}
+
+	if nameNodes[0].FirstChild != nil {
+		name := nameNodes[0].FirstChild.Data
+
+		return cp.PrepareStringChunk(name)
+	}
+
+	return ""
+}
+
+func (p *Parser) parseLongDesc(doc *html.Node) string {
 	var longDesc string
 
-	text = cp.PrepareStringChunk(text)
-	text = strings.Replace(text, "<p>", "", -1)
-	for _, b := range strings.SplitAfter(text, ">") {
-		if len(b) > 0 && b[0] != '<' {
-			longDescLine := strings.Replace(b, "</p>", "", -1)
-			if len(longDescLine) > 0 && (strings.HasPrefix(longDescLine, "Weitere Infos folgen") == false) {
-				longDesc += longDescLine
-				longDesc += " "
+	for _, longDescNode := range p.longDescNodes(doc) {
+
+		for c := longDescNode.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.TextNode {
+				if strings.HasPrefix(c.Data, "Weitere Infos folgen") == false {
+					longDesc += cp.PrepareStringChunk(c.Data)
+					longDesc += " "
+				}
 			}
 		}
 	}
@@ -98,20 +117,70 @@ func (p *Parser) selector(s []string) []string {
 		"body",
 		"div#ja-wrapper",
 		"div#ja-containerwrap-fr",
-		"div#containerwrap2",
+		"div#ja-containerwrap2",
 		"div#ja-container",
 		"div#ja-container2",
 		"div#ja-mainbody-fr",
 		"div#ja-contentwrap",
 		"div#ja-content",
-		"div.article-content",
 	}
 
 	return append(baseSelector, s...)
 }
 
+func (p *Parser) listAnimalNodes(node *html.Node) []*html.Node {
+	var detailNodes []*html.Node
+
+	top := cp.NodeSelect(node, p.selector([]string{
+		"table.blog",
+		"tbody",
+		"tr",
+		"td",
+		"div",
+		"div.contentpaneopen",
+	}))
+
+	rest := cp.NodeSelect(node, p.selector([]string{
+		"table.blog",
+		"tbody",
+		"tr",
+		"td",
+		"table",
+		"tbody",
+		"tr",
+		"td",
+		"div.contentpaneopen",
+	}))
+
+	detailNodes = append(detailNodes, top...)
+
+	return append(detailNodes, rest...)
+}
+
+func (p *Parser) nameNodes(node *html.Node) []*html.Node {
+	return cp.NodeSelect(node, []string{
+		"h2.contentheading",
+	})
+}
+
+func (p *Parser) detailLinkNodes(node *html.Node) []*html.Node {
+	return cp.NodeSelect(node, []string{
+		"div.article-content",
+		"p",
+		"a",
+	})
+}
+
+func (p *Parser) longDescNodes(node *html.Node) []*html.Node {
+	return cp.NodeSelect(node, []string{
+		"div.article-content",
+		"p",
+	})
+}
+
 func (p *Parser) imageNodes(node *html.Node) []*html.Node {
 	return cp.NodeSelect(node, p.selector([]string{
+		"div.article-content",
 		"ul.sig-container",
 		"li.sig-block",
 		"span.sig-link-wrapper",
--- catnip/verlorenwasser/parser_test.go
+++ catnip/verlorenwasser/parser_test.go
@@ -21,7 +21,7 @@ func TestParseList(t *testing.T) {
 	p := NewParser()
 	tests := []ParseListTest{
 		{
-			"test_data/list-cats.xml",
+			"test_data/list-cats.html",
 			[]pb.Animal{
 				pb.Animal{
 					Id:       "iben",
@@ -30,123 +30,16 @@ func TestParseList(t *testing.T) {
 					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2025-iben.html",
 				},
 				pb.Animal{
-					Id:       "amela",
-					Name:     "Amela",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2024-amela.html",
+					Id:       "ingfried",
+					Name:     "Ingfried",
+					LongDesc: "Ingfried verzieht sich in die äußerste Ecke, am liebsten unter den Ofen. Er faucht nicht, man kann ihn hochheben - aber er mag es nicht so. Bei ihm sind wir sicher, dass es wird und er dankbar für ein eigenes Plätzchen wäre, auf dem man ihm die Chance auf ein liebevolles Leben gibt. Ingfried wurde 2008 geboren.",
+					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/1227-ingfried.html",
 				},
 				pb.Animal{
-					Id:       "paddy",
-					Name:     "Paddy",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2023-paddy.html",
-				},
-				pb.Animal{
-					Id:       "abbie",
-					Name:     "Abbie",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2022-abbie.html",
-				},
-				pb.Animal{
-					Id:       "jarko",
-					Name:     "Jarko",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2021-jarko.html",
-				},
-				pb.Animal{
-					Id:       "stina",
-					Name:     "Stina",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2020-stina.html",
-				},
-				pb.Animal{
-					Id:       "annika",
-					Name:     "Annika",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2019-annika.html",
-				},
-				pb.Animal{
-					Id:       "ambra",
-					Name:     "Ambra",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2018-ambra.html",
-				},
-				pb.Animal{
-					Id:       "steen",
-					Name:     "Steen",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2017-stenn.html",
-				},
-				pb.Animal{
-					Id:       "kiri",
-					Name:     "Kiri",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/2016-kiri.html",
-				},
-			},
-		},
-		{
-			"test_data/list-dogs.xml",
-			[]pb.Animal{
-				pb.Animal{
-					Id:       "lino",
-					Name:     "Lino",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2013-lino.html",
-				},
-				pb.Animal{
-					Id:       "tamy",
-					Name:     "Tamy",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2012-tamy.html",
-				},
-				pb.Animal{
-					Id:       "anja",
-					Name:     "Anja",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2011-anja.html",
-				},
-				pb.Animal{
-					Id:       "maja",
-					Name:     "Maja",
-					LongDesc: "",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2010-maja.html",
-				},
-				pb.Animal{
-					Id:       "marlitt",
-					Name:     "Marlitt",
-					LongDesc: "Marlitt ist eine ganz liebe kleine Maus. Manchmal hat es den Anschein, als ob sie die Menschen richtig anlächelt. Anfangs zeigt sie sich mitunter zwar etwas schüchtern, aber wenn man sie näher kennenlernt, baut sie Vertrauen auf. Da sie sich in der Gesellschaft anderer Hunde sehr wohl fühlt, wäre sie als Zweithund gut geeignet. Dann könnte sie sich an einem anderen souveränen Hund gut orientieren. Mit ihren Kumpels spielt und tobt sie ausgiebig. Sie fühlt sich in der Gruppe gut aufgehoben und kommt mit allen gut aus. Auch bei Katzen und Kleintieren zeigt sie sich verträglich. Da sie als Welpe wahrscheinlich vieles nicht kennenglernt hat, fehlt ihr das nötige Selbstvertrauen. So sind ihr manche Geräusche nicht so angenehm. Sie reagiert nicht panisch, aber ein ruhiges Zuhause würde sie bevorzugen. Kleinere Kinder - vor allem hektische und schrille - sollten nicht vorhanden sein. Marlitt wurde 2011 geboren. Sie wiegt 12 kg.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/1798-marlitt.html",
-				},
-				pb.Animal{
-					Id:       "bernice",
-					Name:     "Bernice",
-					LongDesc: "Bernice ist eine ganz liebe und verschmuste Foxterrier-Mischlingshündin. Bei Menschen ist sie sehr anhänglich. Und auch mit Artgenossen versteht sie sich sehr gut. Sowohl der Umgang mit Rüden wie auch Hündinnen sind unproblematisch für sie. Bei Gassitouren ist sie gern vorneweg - und immer mit der Nase am Boden. Sie liebt Schnüffeltouren über alles. Die ausgesprochen freundliche Hündin ist kinderlieb und rundum verträglich. Bernice wurde 2006 geboren und wiegt nur 13,5 kg.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2002-bernice.html",
-				},
-				pb.Animal{
-					Id:       "sonngard",
-					Name:     "Sonngard",
-					LongDesc: "Sonngard sieht auf dem Foto wie ein normaler Schäferhundmix aus - aber sie ist viel kleiner. Die Fotos täuschen: Sonngard wiegt nur 12 kg. Bitte vergleichen Sie die Fotos mit den Fotos anderer Hunde und deren Gewichtsangaben. Die junge Hündin ist sehr freundlich, aber noch eine Spur unsicher. Größere Kinder stellen wahrscheinlich kein Problem für sie dar - bei kleineren könnte es ihr eventuell zu hektisch werden. Sonngard versteht sich mit Rüden und Hündinnen gleichermaßen - könnte also auch gut als Zweithund gehalten werden. Dann gewinnt sie auch mehr Sicherheit und wird nicht so angespannt und nervös. Leinenführigkeit üben wir zur Zeit, denn sie kennt nichts. Sie ist da eher auf dem Stand eines Welpen, obwohl sie schon zwei Jahre alt ist. Aber da sie sehr aufgeweckt ist, wird sie alles schnell lernen.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2001-sonngard.html",
-				},
-				pb.Animal{
-					Id:       "anieke",
-					Name:     "Anieke",
-					LongDesc: "Anieke ist ebenfalls eine kleine Hündin. Sie sieht viel größer auf den Fotos aus. Aber sie wiegt nur 10 kg und ist damit sogar zarter als der Dackelverschnitt Vin mit seinen 13,5 kg. Anieke versteht sich mit Hündinnen und Rüden. Sie lebt gern in der Gruppe unter ihren Artgenossen. Mit Menschen hat sie es allerdings nicht so sehr. Sie zeigt sich bei uns recht scheu und unsicher. Aber wenn die Richtigen kommen, wird sie Vertrauen fassen und sich über ein neues ruhiges Zuhause sehr freuen. Anieke wurde 2012 geboren.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/2000-anieke.html",
-				},
-				pb.Animal{
-					Id:       "vin",
-					Name:     "Vin",
-					LongDesc: "Vin ist ein Dackelmischling. Mit seinen kurzen Beinchen ist er ein lustiges kleines Kerlchen. Wir halten ihn für einen Hund, der auch für Anfänger in der Hundehaltung gut geeignet wäre, denn Vin ist lieb, nett, freundlich, ruhig, ausgeglichen ... Er macht keinem etwas streitig, kämpft nicht um Futter - ist einfach nur zurückhaltend und angenehm. Vin schmust für sein Leben gern und liebt es, gestreichelt zu werden. Er wurde 2005 geboren und wiegt 13,5 kg.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/1999-vin.html",
-				},
-				pb.Animal{
-					Id:       "romina",
-					Name:     "Romina",
-					LongDesc: "Romina ist ein Schnauzer-Pudel-Mischling. Die junge aufgeweckte Hündin ist freundlich und angenehm vom Wesen her. Sie versteht sich auch mit ihren Artgenossen. Romina lebte früher mit mehreren Hunden in einem Haushalt (einer davon ist Pauletta). Allerdings ging es ihr dort nicht so gut. Davon zeugt auch ihr Ohr, das seltsam absteht. Es handelt sich um ein unbehandeltes Blutohr (Bluterguss an der Ohrmuschel, der immer wieder einblutet). Wir haben es bereits operieren lassen und hoffen, es wird sich alles richten. Romina bleibt nicht gern allein. Sie muss sich erst einleben und anpassen. So mag sie auch noch keine Leine, denn richtige Gassitouren kennt sie gar nicht. Aber da Romina freundlich und quirlig-neugierig ist, wird sie das bisher Versäumte schnell nachholen. Wir halten sie für eine gute Familienhündin. Sie wäre auch als Zweithund sehr geeignet. Romina wurde 2011 geboren und wiegt 14 kg.",
-					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/hunde/1998-romina.html",
+					Id:       "panther",
+					Name:     "Panther",
+					LongDesc: "Panther wurde 2005 geboren. Dieser wunderschöne Kater ist sehr gelehrig und unternehmungslustig. Er wäre mit einer reinen Wohnungshaltung nicht zufrieden, sondern möchte auch draußen alles erforschen. Wie man sieht mag er Höhlen, Unterschlüpfe und Verstecke.",
+					URL:      "http://www.tierheim-verlorenwasser.de/unsere-tiere/katzen/88-panther.html",
 				},
 			},
 		},
